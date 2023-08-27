fed100711edd4640f30f429779f4e9bd71342ae7
hoanpm <hoan922003@gmail.com>
2023-08-27 10:08:24 +0700

    fix bug

---
+++ Desktop/Messenger/Messenger/Controllers/NewConversationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewConversationViewController.swift
+//  Messenger
+//
+//  Created by Vũ Hoàn on 09/02/1445 AH.
+//
+
+import UIKit
+
+class NewConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ Desktop/Messenger/Messenger/Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerViewController.swift
+//  Messenger
+//
+//  Created by Vũ Hoàn on 09/02/1445 AH.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
